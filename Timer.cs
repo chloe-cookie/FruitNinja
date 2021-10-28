c73f759656984cf4fc5bbcc55c9d5da49407d118
chloe-cookie <71799672+chloe-cookie@users.noreply.github.com>
2021-10-28 15:07:28 +1100

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float TimeRemaining = 60f;
+    public Text TimeOutput;
+
+    bool TimeEnded = false;
+    float timeRemaining;
+
+    private void OnEnable()
+    {
+        //we setup the variables for our timer.
+
+        TimeEnded = false;
+        timeRemaining = TimeRemaining;
+        SetText();//never repeat code. rule #1
+    }
+
+    void Update()
+    {
+        //only count time, if the game hasn't ended
+        if (!TimeEnded) //TimeEnded == false
+        {
+            timeRemaining -= Time.deltaTime;
+
+            SetText();
+
+            if (timeRemaining <= 0)
+            {//we want to stop
+                TimeEnded = true;
+            }
+        }
+    }
+
+    void SetText()
+    {
+        var finalTime = Mathf.Floor(timeRemaining);
+        TimeOutput.text = finalTime.ToString();
+    }
+}
